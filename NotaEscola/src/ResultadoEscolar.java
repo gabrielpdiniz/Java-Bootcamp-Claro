adc6e02ac144bd5dec8c32d3c40e929667ef5124
Gabriel Peçanha Diniz <gabrielpdiniz02@gmail.com>
2024-10-03 15:37:21 -0300

    Add files via upload

---
+++ NotaEscola/src/ResultadoEscolar.java
@@ -0,0 +1,25 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) throws Exception {
+        int nota = 7;
+
+        //if(nota >= 7)
+        //System.out.println("Aprovado");
+
+        //else if(nota >= 5 && nota < 7)
+        //System.out.println("Prova Recuperaçao");
+
+        //else 
+        //System.out.println("Reprovado");
+        
+        //Ao inves de escrever tudo isso, fazer dessa forma:
+
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+        System.out.println(resultado);
+
+
+
+
+        
+    }
+}
