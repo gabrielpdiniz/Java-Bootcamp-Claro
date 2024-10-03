5a8376624849b5c6e250f4df9ae0d3837440062b
Gabriel Peçanha Diniz <gabrielpdiniz02@gmail.com>
2024-10-03 15:52:23 -0300

    Update SistemaMedida.java

--- SistemaMedida/src/SistemaMedida.java
+++ SistemaMedida/src/SistemaMedida.java
@@ -1,5 +1,16 @@
-public class App {
+public class SistemaMedida {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String sigla = "A";
+
+        /* 
+        if(sigla == "P")
+            System.out.println("PEQUENO");
+        else if(sigla == "M")
+            System.out.println("MEDIO");
+        else if(sigla == "G")
+            System.out.println("GRANDE");
+        else
+            System.out.println("INDEFINIDO");
+         */
     }
 }
