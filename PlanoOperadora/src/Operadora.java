fb69e8c993bfaa187e3a10ed5da2082ff660d0ce
Gabriel Peçanha Diniz <gabrielpdiniz02@gmail.com>
2024-10-07 09:23:17 -0300

    Update Operadora.java

--- PlanoOperadora/src/Operadora.java
+++ PlanoOperadora/src/Operadora.java
@@ -1,5 +1,17 @@
-public class App {
+public class Operadora {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String plano = "T";
+
+        switch (plano) {
+            case "T":{
+                System.out.println("5GB Youtube");
+            }
+            case "M":{
+                System.out.println("Whats e Instagram gratis");
+            }
+            case "B":{
+                System.out.println("100 minutos de ligaçao");
+            }
+        }
     }
 }
