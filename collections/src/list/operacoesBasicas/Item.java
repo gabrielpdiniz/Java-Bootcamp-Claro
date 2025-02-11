74c7d3f38ce350df10734761e807eca3998015c4
Gabriel Peçanha Diniz <gabrielpdiniz02@gmail.com>
2025-02-11 14:54:51 -0300

    Add files via upload

---
+++ collections/src/list/operacoesBasicas/Item.java
@@ -0,0 +1,19 @@
+package list.operacoesBasicas;
+
+public class Item {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Item(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+}
